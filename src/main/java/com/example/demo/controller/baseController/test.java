32ec042614dc5b9d21eff58a85b7eff1a21cc8db
Toky <466350259@qq.com>
2018-09-07 13:03:48 +0800

    toky5

--- src/main/java/com/example/demo/controller/baseController/test.java
+++ src/main/java/com/example/demo/controller/baseController/test.java
@@ -5,8 +5,10 @@ import java.util.Random;
 public class test {
     public static void main(String[] args) {
         Random rand=new Random();
-        for(int i =0;i<(2018-1949);i++){
-            System.out.print(rand.nextInt(300) + 10+",");
+        for(int i =1949;i<2018;i++){
+//            System.out.print(rand.nextInt(300) + 10+",");
+            System.out.print("'"+i+"',");
         }
+
     }
 }
