092febf4ffad82179327bb05792da4d68f545c5d
Toky <466350259@qq.com>
2018-09-07 12:15:25 +0800

    toky5

---
+++ src/main/java/com/example/demo/controller/baseController/test.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller.baseController;
+
+import java.util.Random;
+
+public class test {
+    public static void main(String[] args) {
+        Random rand=new Random();
+        for(int i =0;i<(2018-1949);i++){
+            System.out.print(rand.nextInt(300) + 10+",");
+        }
+    }
+}
